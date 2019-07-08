1410fde1e1948804c163f8bb0b41447e7682eef9
rickpedrosa94@gmail.com <Grievous1234>
2019-07-09 01:07:50 +0200

    Hide all items on calendar menu toolbar done

--- app/src/main/java/com/example/animecalendar/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/animecalendar/ui/calendar/CalendarFragment.java
@@ -105,7 +105,7 @@ public class CalendarFragment extends Fragment implements OnSelectDateListener {
         );
     }
 
-    private void setupRecyclerView(View view) {
+    private void setupRecyclerView(View view) {//TODO USAR TRANSFORMATIONS Y VIEWMODEL DE ACTIVITY
         listEpisodes = ViewCompat.requireViewById(view, R.id.listEpisodesCalendar);
         listAdapter = new CalendarFragmentViewAdapter();
         listAdapter.setOnItemClickListener((view1, position) -> {
